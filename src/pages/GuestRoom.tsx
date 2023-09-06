bf65c9784b6ef858cb01ab0199a39d80a633a5ff
Zbiba <abdrrahmanabdo46@gmail.com>
2023-09-06 17:33:52 +0100

    added chat components

--- src/pages/GuestRoom.tsx
+++ src/pages/GuestRoom.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
 import { Socket } from 'socket.io-client'
+import { Chat } from '../components/Chat'
 
 interface Props {
   socket: Socket
   urls: string[]
+  roomId: string | undefined
   current: string
 }
 
-export const GuestRoom = ({ socket, urls, current }: Props) => {
+export const GuestRoom = ({ socket, urls, roomId, current }: Props) => {
   const [Playing, setPlaying] = useState(false)
 
   useEffect(() => {
@@ -39,7 +41,7 @@ export const GuestRoom = ({ socket, urls, current }: Props) => {
       </div>
       <div className="w-3/12 ">
         <h1 className="ml-2">Playlist :</h1>
-        <div className="w-full h-2/5 overflow-scroll p-1">
+        <div className="w-full h-2/5 overflow-scroll p-1 mb-[52px]">
           {urls.map((url) => {
             const regex = /v=([A-Za-z0-9_-]+)/
             const match = url.match(regex)
@@ -61,9 +63,27 @@ export const GuestRoom = ({ socket, urls, current }: Props) => {
                   <h1 className="text-lg">{url}</h1>
                 </div>
               )
+            } else {
+              return (
+                <div
+                  className={
+                    url === current
+                      ? 'bg-gray-500 flex items-center gap-3 border rounded p-2 m-1 overflow-hidden cursor-pointer'
+                      : 'flex items-center gap-3 border rounded p-2 m-1 overflow-hidden cursor-pointer'
+                  }
+                >
+                  <img
+                    src={`https://i.ytimg.com/vi/a/mqdefault.jpg`}
+                    className="w-10 h-10"
+                    draggable="false"
+                  />
+                  <h1 className="text-lg">{url}</h1>
+                </div>
+              )
             }
           })}
         </div>
+        <Chat socket={socket} roomId={roomId} />
       </div>
     </>
   )
